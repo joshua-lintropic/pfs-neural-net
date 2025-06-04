83ea927f7f2d7c1f6cc9bbd53271563298870ed9
Joshua Lin <46388019+joshua-linsanity@users.noreply.github.com>
2025-06-04 16:41:56 -0400

    updated lossfun1, construct_graph for new loss

--- construct_graph.py
+++ construct_graph.py
@@ -1,65 +1,48 @@
-import torch
-import numpy as np
-import gnn
-import glob
+import torch, numpy as np, gnn, os, glob
 from config import *
-import os
 
-def to_Graph(properties):
-    x_h = torch.zeros(nfibers, n_h).float().cuda()
-    x_g = torch.tensor(properties).float().cuda()
+def to_Graph(class_props: np.ndarray) -> gnn.BipartiteData:
+    n_classes   = class_props.shape[0]
 
-    # all classes are observable by all fibers now
-    num_h = x_h.size(0)
-    num_g = x_g.size(0)
-    e_h = []
-    e_g = []
-    for i in range(num_h):
-        for j in range(num_g):
-            e_h.append(i)
-            e_g.append(j)
-    edge_attr = torch.tensor(edge_attr).float()
-    edge_index = torch.tensor([e_h,e_g],dtype=torch.long)
+    e_h, e_g, edge_attr = [], [], []
+    for h in range(nfibers):
+        for g in range(n_classes):
+            e_h.append(h)
+            e_g.append(g)
+            edge_attr.append(np.zeros(n_x, dtype=np.float32))
 
-    u=torch.tensor([np.zeros(n_u)]).float()
-    data = gnn.BipartiteData(edge_index.cuda(),x_h.cuda(),x_g.cuda(),edge_attr.cuda(),u.cuda())
-    return data
+    edge_index = torch.tensor([e_h, e_g], dtype=torch.long)
+    edge_attr  = torch.tensor(edge_attr).float()
 
-# This function varies from problem to problem. 
-# The following example only works for the PFS problem
-# def to_Graph(indices,properties):
-#     # properties: galaxy properties useful for g nodes
-#     # indices: pre-calculated connectivity
-#     properties = np.array(properties)
-#     edge_attr = []
-#     e_h = [] # start from h nodes
-#     e_g = [] # end at g nodes
+    x_h = torch.zeros(nfibers, n_h)                    # (nfibers × n_h)
+    x_g = torch.tensor(class_props, dtype=torch.float) # (n_g × ?)
+    u   = torch.zeros(1, n_u)
 
-#     # Graph Connectivity Related to the Problem
-#     for i,index in enumerate(indices):
-#         for j in range(len(index)):
-#             if index[j]<2394: 
-#                 e_h.append(index[j])
-#                 e_g.append(k)
-#                 edge_attr.append(np.zeros(n_x)) # Edge initialization
+    return gnn.BipartiteData(edge_index.cuda(), x_h.cuda(), x_g.cuda(), edge_attr.cuda(), u.cuda())
 
-#     edge_attr = torch.tensor(edge_attr).float()
-#     edge_index = torch.tensor([e_h,e_g],dtype=torch.long)
-
-#     x_h = torch.zeros(2394,n_h).float()
-#     x_g = torch.tensor(properties[reachable]).float()
-#     u=torch.tensor([np.zeros(n_u)]).float()
-#     data = gnn.BipartiteData(edge_index.cuda(),x_h.cuda(),x_g.cuda(),edge_attr.cuda(),u.cuda())
-#     return data
 
 if __name__ == '__main__':
-    names = glob.glob('pairs-%s/pair-*'%case) # pre-calculated connectivity
-    utils = np.loadtxt('initial_info/utils-%s.txt'%case) # pre-calculated galaxy properties
+    # class properties file:  utils‑<case>.txt   (n_g rows)
+    # utils = np.loadtxt(f'initial_info/utils-{case}.txt')   # shape (n_g, ≥2)
+
+    utils = np.array([
+        [68.2 * 10**3,  2],
+        [69.3 * 10**3,  2],
+        [96.3 * 10**3,  2],
+        [14.4 * 10**3, 12],
+        [22.0 * 10**3,  6],
+        [ 8.3 * 10**3,  6],
+        [14.0 * 10**3, 12],
+        [22.0 * 10**3,  6],
+        [ 7.4 * 10**3,  3],
+        [ 4.5 * 10**3,  6],
+        [ 2.8 * 10**3, 12],
+        # TODO: each galaxy in Class 12 has indep time req
+        [ 9.7 * 10**3, 15]
+    ])
 
     if not os.path.exists('graphs-%s/'%case):
         os.system('mkdir graphs-%s'%case)
-    for k in range(len(names)):
-        indices = np.loadtxt(names[k],dtype=int)
-        # graph = to_Graph(indices,utils)
-        graph = to_Graph(utils)
-        torch.save(graph,names[k].replace('pairs-%s/pair'%case,'graphs-%s/graph'%case).replace('txt','pt'))
+
+    graph = to_Graph(utils)
+    torch.save(graph, f'graphs-{case}/graph-full.pt')
--- lossfun1.py
+++ lossfun1.py
@@ -1,47 +1,56 @@
-import torch
-import torch.nn as nn
-from torch_scatter import scatter_mean,scatter
-import torch.nn.functional as F
-from config import *
+"""
+Loss for the *counts* formulation   (n_{ik}  instead of   t_{ik})
 
-def Lossfun(time,graph,penalty=0,finaloutput=False):
-    total_time = 42 # total number of visits
-    # Hyperparameter
-    leaky = nn.LeakyReLU(0.) # parameter here tunes the panelty difference between overtime and undertime
-    # Batch
-    bt = graph.batch
-    src,tgt = graph.edge_index
-    x_g = graph.x_g
-    x_h = graph.x_h
-    timereq = x_g[:,0]
-    tgtclass = x_g[:,1:-1]
-    totclass = torch.sum(tgtclass,dim=0)
-    # Time per galaxy
-    time_sum = scatter(time,tgt,dim_size=x_g.size(0),reduce='sum')
-    # Time per fiber
-    spent_time = scatter(time,src,dim_size=x_h.size(0),reduce='sum')
-    # Overtime
-    overtime = spent_time-total_time
-    delta = leaky(overtime).view(1,nfibers) 
-    # Penalty
-    time_constraint = torch.sum(penalty*delta*delta)
-    # Reward
-    sharpness = 5
-    observed = torch.sigmoid((time_sum-timereq+0.5)*sharpness)
-    reward = (observed*tgtclass.T).T
-    completeness = torch.sum(reward,dim=0)/torch.sum(tgtclass,dim=0)
-    totnum = torch.sum(observed)
-    totutils = torch.min(completeness)
-    
+Output tensor `assign` produced by the GNN lives on every edge and is
+interpreted as “how many sources of class g the fibre h will observe”.
+During training we keep it continuous in (0, ∞) but push it towards {0,1}
+through the usual noisy‑sigmoid trick.
+
+Notation inside the code
+------------------------
+assign        : n_{ik}′   (edge‑level variable)
+timereq[g]    : T_i       (exposure time needed by class i)
+totgal[g]     : N_i       (# galaxies in class i)
+"""
+import torch, torch.nn.functional as F
+from torch_scatter import scatter
+# from config import Tmax, sharpness, noiselevel
+
+Tmax = 15
+sharpness = 20
+noiselevel = 0.3 #[0.2,0.3,0.2,0.3][idx]
+
+def Lossfun(assign, graph, penalty=0., finaloutput=False):
+    src, tgt  = graph.edge_index
+    timereq   = graph.x_g[:, 0] 
+    totgal    = graph.x_g[:, 1] 
+
+    if noiselevel > 0:
+        noise = torch.rand_like(assign).sub(0.5).mul(noiselevel)
+    else:
+        noise = 0.
+    assign_hat = torch.sigmoid((assign + noise - 0.5) * sharpness)
+
+    class_cnt = scatter(assign_hat, tgt, dim_size=timereq.size(0), reduce='sum')
+
+    time_per_edge = assign_hat * timereq[tgt]
+    fibre_time    = scatter(time_per_edge, src,
+                             dim_size=graph.x_h.size(0), reduce='sum')
+
+    overtime   = fibre_time - Tmax
+    time_term  = penalty * (F.relu(overtime).pow(2)).sum()
+
+    completeness = class_cnt / totgal.clamp_min(1.)   # avoid /0
+    util        = completeness.min()
+
     if finaloutput:
-        sharpness = 1000
-        observed = torch.sigmoid((time_sum-timereq+0.5)*sharpness)
-        reward = (observed*tgtclass.T).T
-        clsnum = torch.sum(reward,dim=0)
-        clsall = torch.sum(tgtclass,dim=0)
-        completeness = clsnum/clsall
-        totnum = torch.sum(observed)
-        totutils = torch.min(completeness)
-        #print(clsnum) #number observed in each class
-        #print(clsall) #total number in each class
-    return -totutils+time_constraint,totutils,totnum,torch.sum(F.relu(overtime))/(nfibers*total_time),torch.sum(F.relu(-overtime))/(nfibers*total_time)
+        # TODO: modify sharpness? 
+        assign_hat = torch.sigmoid((assign - 0.5) * 1000)
+        class_cnt  = scatter(assign_hat, tgt, dim_size=timereq.size(0), reduce='sum')
+        completeness = class_cnt / totgal.clamp_min(1.)
+        util = completeness.min()
+
+    tot_edges_active = assign_hat.sum()
+
+    return -(util) + time_term, util, tot_edges_active, \
+           F.relu(overtime).mean(), F.relu(-overtime).mean()
