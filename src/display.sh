29ef744befb143e0a4aa17642ff04a34ecbb413d
Joshua Lin <joshua@lintropic.com>
2025-07-14 14:09:51 -0400

    open in same window

--- src/display.sh
+++ src/display.sh
@@ -1,6 +1,7 @@
-open -a Preview ../figures/"$(ls -1 ../figures/A* | sort -r | head -n 1)"
-open -a Preview ../figures/"$(ls -1 ../figures/B* | sort -r | head -n 1)"
-open -a Preview ../figures/"$(ls -1 ../figures/C* | sort -r | head -n 1)"
-open -a Preview ../figures/"$(ls -1 ../figures/D* | sort -r | head -n 1)"
-open -a Preview ../figures/"$(ls -1 ../figures/E* | sort -r | head -n 1)"
+open -a Preview \
+  $(ls -1 ../figures/A* | sort -r | head -n 1) \
+  $(ls -1 ../figures/B* | sort -r | head -n 1) \
+  $(ls -1 ../figures/C* | sort -r | head -n 1) \
+  $(ls -1 ../figures/D* | sort -r | head -n 1) \
+  $(ls -1 ../figures/E* | sort -r | head -n 1)
 
